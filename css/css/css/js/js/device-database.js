081effdf33cf5fb8050a931c12e24b1141331c58
Instamod88 <132873133+Instamod88@users.noreply.github.com>
2026-01-16 13:08:09 -0300

    Add device-database.js with device list

---
+++ css/css/css/js/js/device-database.js
@@ -0,0 +1,11 @@
+const dispositivos = [
+  "Samsung Galaxy S23",
+  "Samsung Galaxy A14",
+  "iPhone 11",
+  "iPhone 12",
+  "iPhone 13",
+  "Redmi Note 12",
+  "POCO X5",
+  "Motorola G54",
+  "Infinix Zero 30"
+];
